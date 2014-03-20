7b51d739b8906ad28637b7d1cc2ccaee3253b297
lishuangjiang <li0shuangjiang@gmail.com>
2014-03-20 17:05:05 +0800

    base structure

---
+++ manage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from flask.ext.script import Manager, Server
+
+from hifcampus import create_app
+
+app = create_app()
+manager = Manager(app)
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0',
+    port = 8000)
+)
+
+if __name__ == "__main__":
+    print 'start manager'
+    manager.run()
