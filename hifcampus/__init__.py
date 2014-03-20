5eaee50ab15a06edef1e1cd55db4c4dd24f1d791
lishuangjiang <li0shuangjiang@gmail.com>
2014-03-20 17:58:08 +0800

    create app

--- hifcampus/__init__.py
+++ hifcampus/__init__.py
@@ -2,7 +2,12 @@
 # encoding: utf-8
 
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile("config.py")
+    db.init_app(app)
     return app
---
+++ hifcampus/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+MONGODB_SETTINGS ={
+        "DB":"HifTest",
+        "host":"localhost",
+        "port":10001,
+        }
